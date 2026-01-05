249e0a622963e2f03f7f489006d70409f6accd51
s9213github <160514205+s9213github@users.noreply.github.com>
2026-01-05 13:48:32 +0530

    Update 01_data_understanding.sql

--- sql/01_data_understanding.sql
+++ sql/01_data_understanding.sql
@@ -1,1 +1,14 @@
 -- Data understanding queries
+-- Total number of orders
+SELECT COUNT(*) FROM `Target_Cs.orders`;
+
+-- Date range of orders
+SELECT 
+  MIN(DATE(order_purchase_timestamp)) AS first_order,
+  MAX(DATE(order_purchase_timestamp)) AS last_order
+FROM `Target_Cs.orders`;
+
+-- Number of customers by state
+SELECT customer_state, COUNT(DISTINCT customer_id)
+FROM `Target_Cs.customers`
+GROUP BY customer_state;
