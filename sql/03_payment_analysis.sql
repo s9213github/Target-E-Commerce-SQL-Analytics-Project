acfa2e86fa13caf2e0fd8c98a393a31e648e2134
s9213github <160514205+s9213github@users.noreply.github.com>
2026-01-05 14:08:59 +0530

    Add payment analysis queries
    Added SQL queries to analyze orders by payment type and installments.

--- sql/03_payment_analysis.sql
+++ sql/03_payment_analysis.sql
@@ -1,1 +1,19 @@
+#Find the month on month no. of orders placed using different payment types.
+with order_payment as
+(select o.order_id,extract(year from o.order_purchase_timestamp) order_year,
+extract(month from o.order_purchase_timestamp) order_month,
+p.payment_type
+from `Target_Cs.orders` o
+join  `Target_Cs.payments` p
+on o.order_id = p.order_id
+)
+select payment_type,order_year,order_month,count(distinct order_id) no_of_orders
+from order_payment
+group by 1,2,3
+order by 1,2,3;
 
+#Find the no. of orders placed on the basis of the payment installments that have been paid.
+select payment_installments,count(distinct order_id) no_of_orders
+from `Target_Cs.payments` 
+group by payment_installments
+order by 1;
